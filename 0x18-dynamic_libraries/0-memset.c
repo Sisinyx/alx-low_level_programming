307fd361ef240425009ff636ad9ab0b14521707c
simret yosef <simretyosef@gmail.com>
2022-12-16 01:53:37 -0800

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset -fills the first n bytes of memory area pointed
+ * to by s with a constant byte b
+ * @s: address to memory block
+ * @b: char to be used
+ * @n: no of bytes to be used
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{	s[n - 1] = b;
+		n--;
+	}
+	return (s);
---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - function copies n bytes from memory
+ * area src to memory area dest
+ * @dest: memory area to be copied to
+ * @src: memory area to be copied from
+ * @n: numberof bytes to be copied
+ *
+ * Return: a pointer to dest(copied to memory block)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
